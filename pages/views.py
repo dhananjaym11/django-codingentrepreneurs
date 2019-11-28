561ba3d36f5b4846af259a7e4a966fcf097ef8b6
Dhananjay Mane <dhananjay.mane@xoriant.com>
2019-11-28 16:16:37 +0530

    Added templates

--- pages/views.py
+++ pages/views.py
@@ -3,8 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    user = request.user
-    return render(request,'home.html',{user: user})
+    my_context = {
+        'my_text': 'Hello World',
+        'my_list': [10,20,30,'a']
+    }
+    return render(request,'home.html',my_context)
     
 def contact_view(request, *args, **kwargs):
-    return HttpResponse('<h1>Contact Page</h1>')
+    return render(request,'contact.html',{})
+    
+def about_view(request, *args, **kwargs):
+    return HttpResponse('<h1>About Page</h1>')
--- project/urls.py
+++ project/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from pages.views import home_view, contact_view
+from pages.views import home_view, contact_view, about_view
 
 urlpatterns = [
     url(r'^$', home_view, name='home'),
     url('contact/', contact_view),
+    url('about/', about_view),
     url(r'^admin/', admin.site.urls),
 ]
